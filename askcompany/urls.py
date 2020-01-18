955f90744d8fd78cbd945803ffb26760cbd4e7af
teclados <wjdtdmwo07@daum.net>
2020-01-18 09:59:40 +0900

    '14강'

---
+++ askcompany/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.http import HttpResponse
+from django.urls import path
+
+def mysum(request, x, y):
+    result = x+y
+    return HttpResponse('result={}'.format(result))
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('mysum/<int:x>/<int:y>', mysum)
+]

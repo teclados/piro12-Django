a53935f9e6db7efe83be5dc6be38c81e16128500
teclados <wjdtdmwo07@daum.net>
2020-01-20 20:41:28 +0900

    12장고 Admin을 통한 데이터 관리

--- shop/models.py
+++ shop/models.py
@@ -5,5 +5,9 @@ class Item(models.Model):
     name = models.CharField(max_length=100)
     desc = models.TextField(blank=True)
     price = models.PositiveIntegerField()
+    is_publish = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '<{}> {}'.format(self.pk, self.name)

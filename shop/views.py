dc1fab952d3a5064e07ee7bb39fac2a475fe2d10
teclados <wjdtdmwo07@daum.net>
2020-01-20 21:35:03 +0900

    13모델을 통한 데이터 조회

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from shop.models import Item
 
 
 def archives_year(request, year):
     return HttpResponse('{}년도에 대한 내용'.format(year))
+
+
+def item_list(request):
+    qs = Item.objects.all()
+
+    q = request.GET.get('q', '')
+    if q:
+        qs = qs.filter(name__icontains=q)
+
+    return render(request, 'shop/item.html', {
+        'item_list': qs,
+        'q': q,
+    })

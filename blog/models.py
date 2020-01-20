aebf215fce71e4aec66860eda7d1a59e61220c44
teclados <wjdtdmwo07@daum.net>
2020-01-21 06:01:38 +0900

    26Media 파일을 다루는 방법

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from askcompany.utils import uuid_upload_to
 
 
 class Post(models.Model):
     author_name = models.CharField(max_length=20)
     title = models.CharField(max_length=100)
     content = models.TextField()
+    photo = models.ImageField(upload_to=uuid_upload_to)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
 

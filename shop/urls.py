3b7f9ba462812d140957aa8aafa570111041d450
teclados <wjdtmdwo07@daum.net>
2020-01-24 11:21:06 +0900

    29장고 Form을 쓰지 않고, 글 생성/수정 구현하기

--- shop/urls.py
+++ shop/urls.py
@@ -9,6 +9,8 @@ app_name = 'shop'
 
 urlpatterns = [
     path('archives/<yyyy:year>/', views.archives_year, name='archives_year'),
-    path('', views.item_list, name='item_detail'),
+    path('', views.item_list, name='item_list'),
     path('<int:pk>/', views.item_detail, name='item_detail'),
+    path('new/', views.item_new, name='item_new'),
+    path('<int:pk>/edit/', views.item_edit, name='item_edit'),
 ]
